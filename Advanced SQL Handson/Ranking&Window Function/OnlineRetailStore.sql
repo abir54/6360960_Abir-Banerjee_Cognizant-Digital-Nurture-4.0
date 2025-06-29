ee1921bf86b8625f2615697d28cda7eed145f733
abir54 <130598525+abir54@users.noreply.github.com>
2025-06-29 18:49:15 +0530

    Add files via upload

---
+++ Advanced SQL Handson/Ranking&Window Function/OnlineRetailStore.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS OnlineRetailStore;
+
+CREATE TABLE OnlineRetailStore (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price DECIMAL(10, 2),
+    StockQuantity INT,
+    IsAvailable BIT
+);
+
+INSERT INTO OnlineRetailStore (ProductID, ProductName, Category, Price, StockQuantity, IsAvailable)
+VALUES
+(1, 'Wireless Mouse', 'Electronics', 799.00, 150, 1),
+(2, 'Bluetooth Speaker', 'Electronics', 1299.50, 85, 1),
+(3, 'Yoga Mat', 'Fitness', 499.99, 60, 1),
+(4, 'LED Desk Lamp', 'Home Decor', 649.00, 100, 1),
+(5, 'Smartphone Stand', 'Accessories', 299.00, 40, 1),
+(6, 'Water Bottle', 'Fitness', 199.00, 20, 1),
+(7, 'Notebook Pack', 'Stationery', 350.00, 0, 0),
+(8, 'USB Type-C Cable', 'Electronics', 249.00, 75, 1),
+(9, 'Wireless Keyboard', 'Electronics', 1599.00, 30, 1),
+(10, 'Table Plant', 'Home Decor', 599.00, 10, 1);
+
+SELECT * FROM OnlineRetailStore;
+
---
+++ Advanced SQL Handson/Stored Procedure/StoredProcedureData.sql
@@ -0,0 +1,14 @@
+-- Separate batch for stored procedure creation
+GO
+CREATE PROCEDURE sp_GetEmployeeCountByDepartment
+    @DepartmentID INT
+AS
+BEGIN
+    SELECT 
+        COUNT(*) AS TotalEmployees
+    FROM Employees
+    WHERE DepartmentID = @DepartmentID;
+END;
+
+GO
+EXEC sp_GetEmployeeCountByDepartment @DepartmentID = 4;
